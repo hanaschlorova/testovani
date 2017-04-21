d9bbdda9ec56ea924e69947d920a1613ecce859b
Schlorova <hschloro@w2k.feico.com>
2017-04-21 13:59:15 +0200

    dnesni verze z hodiny

---
+++ testy.py
@@ -0,0 +1,25 @@
+def secti(a, b):
+    return a + b
+
+
+def nasob(a, b):
+    raise Exception('Funkce jeste neni implementovana!')
+
+
+def vydel(a, b):
+    out = a / b
+    return out
+
+def test_secti():
+    assert secti(1, 2) == 3
+
+
+
+
+import pytest
+
+
+def test_deleni_nulou():
+    with pytest.raises(Exception):
+        vydel(1,0)
+
